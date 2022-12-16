ece04479611d1ff9edbe9c8e6a349f19ec2d4f5d
Andrey Engo <5522662@gmail.com>
2022-12-16 14:50:44 +0300

    add Homework4

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+using static System.Console;
+
+WriteLine("Введите число - ");
+
+int n = int.Parse(ReadLine()!);
+int count = 1;
+
+while (count != n){
+    if (count%2 == 0) WriteLine(count);
+    count += 1;
+}
+
