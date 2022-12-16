01e3ef46b21dbb6abd5b1f3c5e19808cd3259d27
Andrey Engo <5522662@gmail.com>
2022-12-16 14:38:50 +0300

    add Homework3

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+
+using static System.Console;
+
+WriteLine("Введите число - ");
+int a = int.Parse(ReadLine()!);
+
+if (a%2 == 0) WriteLine($" Число {a} чётное");
+else WriteLine($" Число {a}  не чётное");
+
